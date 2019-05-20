72757a8789864e8cb85f9e1a5d28ce97004c203c
liangxiong <scliangxiong@gmail.com>
2019-05-20 21:43:51 +0800

    <feat><annotation>:<update doc and refactor code>

--- review-annotation/src/main/java/org/liangxiong/demo/spring/config/WebConfiguration.java
+++ review-annotation/src/main/java/org/liangxiong/demo/spring/config/WebConfiguration.java
@@ -1,8 +1,8 @@
 package org.liangxiong.demo.spring.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -21,12 +21,11 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
      *
      * @return
      */
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/");
         viewResolver.setSuffix(".jsp");
-        return viewResolver;
+        registry.viewResolver(viewResolver);
     }
-
 }
