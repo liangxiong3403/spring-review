1febb2a79b2a8334b24987549d874b47f11ce80d
liangxiong <1071608617@qq.com>
2019-05-24 16:08:27 +0800

    <feat><bug>:<chang jackson version to fix bug>

---
+++ README.md
@@ -0,0 +1,4 @@
+- The journey of a thousand miles starts with one step(千里之行,始于足下)
+
+> reveiw spring basic concepts
+
--- review-xml/src/main/java/org/liangxiong/demo/spring/aop/UserAspect.java
+++ review-xml/src/main/java/org/liangxiong/demo/spring/aop/UserAspect.java
@@ -1,13 +1,31 @@
 package org.liangxiong.demo.spring.aop;
 
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 /**
  * @author liangxiong
  * @Date:2019-05-23
  * @Time:10:16
- * @Description
+ * @Description 自定义切面
  */
 @Aspect
 public class UserAspect {
+
+    /**
+     * 洗手操作
+     */
+    @Before("execution(* org.liangxiong.demo.spring.service.impl.*.eat(..))")
+    public void washHands() {
+        System.out.println("wash hands before eat meat");
+    }
+
+    /**
+     * 散步操作
+     */
+    @AfterReturning("execution(* org.liangxiong.demo.spring.service.impl.*.eat(..))")
+    public void walk() {
+        System.out.println("go for a walk after eating meat");
+    }
 }
