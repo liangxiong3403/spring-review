21fb93efd4991dcaa9ee4c38a81923b8451db021
liangxiong <1071608617@qq.com>
2019-05-24 16:01:12 +0800

    <feat><aspectj>:<introduction of aspectj>

---
+++ review-xml/src/main/java/org/liangxiong/demo/spring/aop/UserAspect.java
@@ -0,0 +1,13 @@
+package org.liangxiong.demo.spring.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * @author liangxiong
+ * @Date:2019-05-23
+ * @Time:10:16
+ * @Description
+ */
+@Aspect
+public class UserAspect {
+}
