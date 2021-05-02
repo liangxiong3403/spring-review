cfe742c7b821085b3e59ecf98e160fc400b31320
liangxiong <scliangxiong@gmail.com>
2021-05-02 11:16:09 +0800

    update dependency for handle CVE issues

---
+++ review-ioc/src/main/java/org/liangxiong/demo/bootstrap/Test.java
@@ -0,0 +1,20 @@
+package org.liangxiong.demo.bootstrap;
+
+/**
+ * @author liangxiong
+ * @date 2020-05-30 16:32
+ * @description
+ **/
+public class Test {
+
+    private static Integer 最大年龄 = 50;
+
+    public static void main(String[] args) {
+        你好();
+    }
+
+    private static void 你好(){
+        System.out.println("hello");
+        System.out.println("age: " + 最大年龄);
+    }
+}
